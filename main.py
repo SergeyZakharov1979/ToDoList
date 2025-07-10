d8755a9c4ad0f15c1e3057c9589a26792b83bf91
SergeyZakharov1979 <sergun.zahar79@gmail.com>
2025-07-10 20:44:41 +0700

    Улучшение логики работы кода

--- main.py
+++ main.py
@@ -2,86 +2,82 @@ import os
 import json
 
 
-def main():
-    class ToDoList:
-        def __init__(self, tasks=None, filename='list.json'):
-            self.tasks = tasks if tasks is not None else []
-            self.filename = filename
+class ToDoList:
+    def __init__(self, tasks=None, filename='list.json'):
+        self.tasks = tasks if tasks is not None else []
+        self.filename = filename
 
-        def create_task_list(self):
-            print(
-                'Чтобы создать лист задач нажмите любую кнопку. Для выхода введите: exit')
-            next_step = input()
-            if next_step == 'exit':
-                exit()
+    def load_tasks(self):
+        with open(self.filename, 'r', encoding='utf-8') as file:
+            self.tasks = json.load(file)
+
+    def save_tasks(self):
+        with open(self.filename, 'w', encoding='utf-8') as file:
+            json.dump(self.tasks, file, ensure_ascii=False, indent=2)
+
+    def create_task_list(self):
+        print(
+            'Чтобы создать лист задач нажмите любую клавишу. Для выхода введите: exit')
+        while (text := input('Введите задачу: ')) != 'exit':
+            task = dict(text=text.capitalize(), status='не завершена')
+            self.tasks.append(task)
+        else:
+            self.save_tasks()
+
+    def add_task(self):
+        text = input('Введите задачу: ')
+        task = dict(text=text.capitalize(), status='не завершена')
+        self.tasks.append(task)
+        self.save_tasks()
+
+    def read_list(self):
+        self.load_tasks()
+        if len(self.tasks) == 0:
+            print('Список задач пуст')
+        else:
+            for i, dict_task in enumerate(self.tasks, 1):
+                print(
+                    f'{i}) {dict_task.get('text')} статус: {dict_task.get('status')}')
+
+    def delete_task(self):
+        task_to_be_deleted = int(input(
+            'Введите номер задачи для удаления: '))
+        self.load_tasks()
+
+        i = 0
+        while i < len(self.tasks):
+            if i == task_to_be_deleted - 1:
+                del self.tasks[i]
+                print('Задача удалена')
+                break
             else:
-                self.add_task()
+                i += 1
+        else:
+            print('Задача в списке не найдена')
+        self.save_tasks()
 
-        def add_task(self):
-            while (text := input('Введите задачу: ')) != 'exit':
-                task = dict(text=text.capitalize(), status='не завершена')
-                self.tasks.append(task)
+    def task_completed(self):
+        task_completed = int(input(
+            'Введите номер завершённой задачи: '))
+        self.load_tasks()
+        i = 0
+        while i < len(self.tasks):
+            if i == task_completed - 1:
+                self.tasks[i]['status'] = 'завершена'
+                print('Задача отмечена, как выполненная')
+                break
             else:
-                with open(self.filename, 'w', encoding='utf-8') as file:
-                    json.dump(self.tasks, file, ensure_ascii=False, indent=2)
+                i += 1
+        else:
+            print('Задача в списке не найдена')
+        self.save_tasks()
 
-        def read_list(self):
-            with open(self.filename, 'r', encoding='utf-8') as file:
-                self.tasks = json.load(file)
-                if len(self.tasks) == 0:
-                    print('Список задач пуст')
-                else:
-                    for i, dict_task in enumerate(self.tasks, 1):
-                        print(
-                            f'{i}) {dict_task.get('text')} статус: {dict_task.get('status')}')
+    def menu(self):
 
-        def delete_task(self):
-            task_to_be_deleted = input(
-                'Введите задачу для удаления: ').capitalize()
-            with open(self.filename, 'r', encoding='utf-8') as input_file:
-                self.tasks = json.load(input_file)
+        comands = {'1': self.create_task_list, '2': self.add_task, '3': self.read_list,
+                   '4': self.task_completed, '5': self.delete_task, '6': exit}
 
-            i = 0
-            while i < len(self.tasks):
-                if self.tasks[i]['text'] == task_to_be_deleted:
-                    del self.tasks[i]
-                    print('Задача удалена')
-                    break
-                else:
-                    i += 1
-            else:
-                print('Задача в списке не найдена')
-
-            with open(self.filename, 'w', encoding='utf-8') as output_file:
-                json.dump(self.tasks, output_file,
-                          ensure_ascii=False, indent=2)
-
-        def task_completed(self):
-            task_completed = input(
-                'Введите завершённую задачу: ').capitalize()
-            with open(self.filename, 'r', encoding='utf-8') as input_file:
-                self.tasks = json.load(input_file)
-            i = 0
-            while i < len(self.tasks):
-                if self.tasks[i]['text'] == task_completed:
-                    self.tasks[i]['status'] = 'завершена'
-                    print('Задача отмечена, как выполненная')
-                    break
-                else:
-                    i += 1
-            else:
-                print('Задача в списке не найдена')
-
-            with open(self.filename, 'w', encoding='utf-8') as output_file:
-                json.dump(self.tasks, output_file,
-                          ensure_ascii=False, indent=2)
-
-        def menu(self):
-
-            comands = {1: self.create_task_list, 2: self.add_task, 3: self.read_list,
-                       4: self.task_completed, 5: self.delete_task, 6: exit}
-
-            print('''===== To-Do List Menu =====
+        print('''===== To-Do List Menu =====
 1. Создать список задач
 2. Добавить задачу
 3. Вывести список задач
@@ -89,8 +85,14 @@ def main():
 5. Удалить задачу
 6. Выйти
 ''')
-            while (waiting_for_command := int(input())) != 6:
-                comands[waiting_for_command]()
+        while (comand := input()) != '6':
+            if comand not in comands:
+                print('Некорректный ввод')
+            else:
+                comands[comand]()
+
+
+def main():
 
     my_todo = ToDoList()
     my_todo.menu()
