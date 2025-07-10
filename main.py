5bf03b2217100ef71798194bd233aee8e9c6f544
SergeyZakharov1979 <sergun.zahar79@gmail.com>
2025-07-10 19:12:20 +0700

    Сделал сырую версию ToDo

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def main():
 
         def add_task(self):
             while (text := input('Введите задачу: ')) != 'exit':
-                task = dict(text=text, status=False)
+                task = dict(text=text.capitalize(), status='не завершена')
                 self.tasks.append(task)
             else:
                 with open(self.filename, 'w', encoding='utf-8') as file:
@@ -28,18 +28,72 @@ def main():
         def read_list(self):
             with open(self.filename, 'r', encoding='utf-8') as file:
                 self.tasks = json.load(file)
-                for i, dict_task in enumerate(self.tasks, 1):
-                    print(f'{i}) {dict_task.get('text')} статус: {dict_task.get('status')}')
+                if len(self.tasks) == 0:
+                    print('Список задач пуст')
+                else:
+                    for i, dict_task in enumerate(self.tasks, 1):
+                        print(
+                            f'{i}) {dict_task.get('text')} статус: {dict_task.get('status')}')
 
         def delete_task(self):
-            pass
+            task_to_be_deleted = input(
+                'Введите задачу для удаления: ').capitalize()
+            with open(self.filename, 'r', encoding='utf-8') as input_file:
+                self.tasks = json.load(input_file)
+
+            i = 0
+            while i < len(self.tasks):
+                if self.tasks[i]['text'] == task_to_be_deleted:
+                    del self.tasks[i]
+                    print('Задача удалена')
+                    break
+                else:
+                    i += 1
+            else:
+                print('Задача в списке не найдена')
+
+            with open(self.filename, 'w', encoding='utf-8') as output_file:
+                json.dump(self.tasks, output_file,
+                          ensure_ascii=False, indent=2)
 
         def task_completed(self):
-            pass
+            task_completed = input(
+                'Введите завершённую задачу: ').capitalize()
+            with open(self.filename, 'r', encoding='utf-8') as input_file:
+                self.tasks = json.load(input_file)
+            i = 0
+            while i < len(self.tasks):
+                if self.tasks[i]['text'] == task_completed:
+                    self.tasks[i]['status'] = 'завершена'
+                    print('Задача отмечена, как выполненная')
+                    break
+                else:
+                    i += 1
+            else:
+                print('Задача в списке не найдена')
+
+            with open(self.filename, 'w', encoding='utf-8') as output_file:
+                json.dump(self.tasks, output_file,
+                          ensure_ascii=False, indent=2)
+
+        def menu(self):
+
+            comands = {1: self.create_task_list, 2: self.add_task, 3: self.read_list,
+                       4: self.task_completed, 5: self.delete_task, 6: exit}
+
+            print('''===== To-Do List Menu =====
+1. Создать список задач
+2. Добавить задачу
+3. Вывести список задач
+4. Отметить задачу как выполненную
+5. Удалить задачу
+6. Выйти
+''')
+            while (waiting_for_command := int(input())) != 6:
+                comands[waiting_for_command]()
 
     my_todo = ToDoList()
-    my_todo.create_task_list()
-    my_todo.read_list()
+    my_todo.menu()
 
 
 if __name__ == "__main__":
